c7fbf397780db801b7ba3a682d0cfccf0f24e8e8
BobbieLuring <128726799+BobbieLuring@users.noreply.github.com>
2024-10-15 18:55:47 +0200

    kodstädning

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const shapes = [
 
 let dark = true
 
+// ----- Kod för formerna -----
+
 function createCorrectShape(index) {
     shapes[index].create('mål', 'shapes')
     shapes[index].create('formen1', 'formen')
@@ -25,10 +27,11 @@ function createIncorrectShapes(indexArr) {
     }
 }
 
+// skapar de nya formerna och tömmer de gamla
 function newShapes() {
     var arr = [];
     while(arr.length < 3){
-        var r = Math.floor(Math.random() * 5);
+        var r = Math.floor(Math.random() * 6);
         if(arr.indexOf(r) === -1) {
             arr.push(r);
         }
@@ -39,8 +42,9 @@ function newShapes() {
     createIncorrectShapes(arr);
 }
 
+// ------- kod för darkmode ------
+
 function toggleDark() {
-    console.log('toggle')
     if (dark === true) {
         document.body.style.backgroundColor = 'white'
         document.body.style.color = 'black'
@@ -53,13 +57,16 @@ function toggleDark() {
     newShapes()
 }
 
+// när sidan har laddats
+
 addEventListener("load", (event) => {
-    console.log('load')
     initCookie()
     newShapes();
     dragElement(document.getElementById("formen"));
 
 });
+
+// ------ kod för kakor ------
 
 function initCookie() {
     let cookiee = document.cookie;
@@ -69,9 +76,9 @@ function initCookie() {
     updateScoreBoard()
 }
 
+// uppdaterar totalen
 function updateScoreBoard() {
     let totalValue = getCookieValue("total");
-    console.log('i functionen')
     if (totalValue) {
         var paragraph = document.getElementById("p");
         paragraph.innerHTML = ''
@@ -99,23 +106,19 @@ function getCookieValue(name) {
     return null; // Return null if the cookie is not found
 }
 
-// Make the DIV element draggable:
-// dragElement(document.getElementById("formen"));
-
+// kod för att dra formen och även logik för att kolla om den är i korrekt form
 function dragElement(elmnt) {
 
     let startpos = getGoalCoordinates('formen');
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elmnt.id)) {
-        // if present, the header is where you move the DIV from:
+        // kanske kan ta bort denna if
         document.getElementById(elmnt.id).onmousedown = dragMouseDown;
     } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
     }
 
     function dragMouseDown(e) {
-        console.log('drag')
         e = e || window.event;
         e.preventDefault();
         // get the mouse cursor position at startup:
@@ -139,12 +142,14 @@ function dragElement(elmnt) {
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
 
+    // ger koordinater för den flyttbara formen
     function getMoveableCoordinates() {
         let element = document.getElementById('formen')
         let rect = element.getBoundingClientRect();
         return rect;
     }
 
+    // ger koordinater för formen som är mål
     function getGoalCoordinates(id) {
         let element = document.getElementById(id)
         let rect = element.getBoundingClientRect();
@@ -157,14 +162,14 @@ function dragElement(elmnt) {
         let moveAbleCoordinates = getMoveableCoordinates();
         let diffX = goalCoordinates.x - moveAbleCoordinates.x
         let diffY = goalCoordinates.y - moveAbleCoordinates.y
-        if ((diffX < 40 && diffX > -40) && (diffY < 40 && diffY > - 40)) {
-            increaseCookie()
+        // hur nära man kan släppa formen och det räknas
+        if ((diffX < 30 && diffX > -30) && (diffY < 30 && diffY > - 30)) {
             moveElmentToGoal(goalCoordinates.x, goalCoordinates.y);
             let element = document.getElementById('formen1')
             element.classList.toggle('animation')
             element.addEventListener('animationend', () => {
-                console.log('animation ended')
                 getNewShape();
+                increaseCookie()
             })
         }
         
@@ -172,12 +177,14 @@ function dragElement(elmnt) {
         document.onmousemove = null;
     }
 
+    // flyttar dragbara formen till samma position som målet
     function moveElmentToGoal(posx, posy) {
         let element = document.getElementById('formen')
         element.style.top = (posy) + "px";
         element.style.left = (posx) + "px";
     }
 
+    // återställer formen till orginalplats
     function getNewShape() {
         let element = document.getElementById('formen')
         element.style.top = (startpos.y) + "px";
@@ -190,207 +197,89 @@ function dragElement(elmnt) {
     }
 }
 
-function createCircleSVG(shapeID, parentID) {
-    // Create an SVG namespace
-    const svgNS = "http://www.w3.org/2000/svg";
-
-    // Create an SVG element
-    const svg = document.createElementNS(svgNS, "svg");
+function standardSVGSettings(shape, parentID, shapeID, svg) {
     svg.setAttribute("width", "100");
     svg.setAttribute("height", "100");
+    shape.setAttribute("stroke-width", "2");
+
+    if (parentID === 'shapes') {
+        dark ? shape.setAttribute("fill", "black") : shape.setAttribute("fill", "white")
+        shape.setAttribute("stroke", "gray");
+    } else {
+        shape.setAttribute("fill", "orange");
+    }
+
+    svg.id = shapeID
+    svg.appendChild(shape);
+    document.getElementById(parentID).appendChild(svg);
+}
+
+function createCircleSVG(shapeID, parentID) {
+
+    const svgNS = "http://www.w3.org/2000/svg";
+    const svg = document.createElementNS(svgNS, "svg");
     
-    // Create a circle element
-    const circle = document.createElementNS(svgNS, "circle");
+    let circle = document.createElementNS(svgNS, "circle");
     circle.setAttribute("cx", "50");
     circle.setAttribute("cy", "50");
     circle.setAttribute("r", "45");
-    circle.setAttribute("stroke-width", "2");
-    if (parentID === 'shapes') {
-        circle.setAttribute("fill", "black");
-        dark ? circle.setAttribute("fill", "black") : circle.setAttribute("fill", "white")
-        circle.setAttribute("stroke", "gray");
-    } else {
-        circle.setAttribute("fill", "orange");
-    }
-    svg.id = shapeID;
-    
-    // Append the circle to the SVG
-    svg.appendChild(circle);
-    
-    // Append the SVG to the div
-    document.getElementById(parentID).appendChild(svg);
+    circle = standardSVGSettings(circle, parentID, shapeID, svg)
 }
 
 function createSVGTriangle(shapeID, parentID) {
-    // Create an SVG namespace element
+
     const svgNS = "http://www.w3.org/2000/svg";
-
-    // Create an SVG element
     const svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "100");
-    svg.setAttribute("height", "100");
 
-    // Create a polygon for the triangle
-    const triangle = document.createElementNS(svgNS, "polygon");
-
-    // Define the points of the triangle (x1,y1 x2,y2 x3,y3)
+    let triangle = document.createElementNS(svgNS, "polygon");
     const points = "50,5 5,99 95,99";
     triangle.setAttribute("points", points);
-
-    // Set a fill color and stroke for the triangle
-    // triangle.setAttribute("fill", "white");
-    // triangle.setAttribute("stroke", "black");
-    triangle.setAttribute("stroke-width", "2")
-    if (parentID === 'shapes') {
-        dark ? triangle.setAttribute("fill", "black") : triangle.setAttribute("fill", "white")
-        triangle.setAttribute("stroke", "gray");
-    } else {
-        triangle.setAttribute("fill", "orange");
-    }
-    svg.id = shapeID
-
-    // Append the triangle to the SVG element
-    svg.appendChild(triangle);
-
-    // Append the SVG to the container in the HTML
-    document.getElementById(parentID).appendChild(svg);
+    triangle = standardSVGSettings(triangle, parentID, shapeID, svg)
 }
 
 function createSVGStar(shapeID, parentID) {
-    // Create an SVG namespace element
+
     const svgNS = "http://www.w3.org/2000/svg";
-
-    // Create an SVG element
     const svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "100");
-    svg.setAttribute("height", "100");
 
-    // Create a polygon for the star
-    const star = document.createElementNS(svgNS, "polygon");
-
-    // Define the points of the 5-pointed star
-    // These are calculated to fit within a 100x100 box
+    let star = document.createElementNS(svgNS, "polygon");
     const points = "50,5 61,38 95,38 68,57 78,91 50,72 22,91 32,57 5,38 39,38"
-
     star.setAttribute("points", points);
-
-    // Set a fill color and stroke for the star
-    star.setAttribute("stroke-width", "2");
-    if (parentID === 'shapes') {
-        dark ? star.setAttribute("fill", "black") : star.setAttribute("fill", "white")
-        star.setAttribute("stroke", "gray");
-    } else {
-        star.setAttribute("fill", "orange");
-    }
-    svg.id = shapeID
-
-    // Append the star to the SVG element
-    svg.appendChild(star);
-
-    // Append the SVG to the container in the HTML
-    document.getElementById(parentID).appendChild(svg);
+    star = standardSVGSettings(star, parentID, shapeID, svg)
 }
 
 function createSVGHexagon(shapeID, parentID) {
-    // Create an SVG namespace element
+
     const svgNS = "http://www.w3.org/2000/svg";
-
-    // Create an SVG element
     const svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "100");
-    svg.setAttribute("height", "100");
 
-    // Create a polygon for the hexagon
-    const hexagon = document.createElementNS(svgNS, "polygon");
-
-    // Define the points of the hexagon that fills the 100x100 box
-    // The hexagon's center is at (50,50), with a radius that reaches the box edges
+    let hexagon = document.createElementNS(svgNS, "polygon");
     const points = "50,5 90,25 90,75 50,95 10,75 10,25";
-
     hexagon.setAttribute("points", points);
-
-    // Set a fill color and stroke for the hexagon
-    hexagon.setAttribute("stroke-width", "2");
-    if (parentID === 'shapes') {
-        dark ? hexagon.setAttribute("fill", "black") : hexagon.setAttribute("fill", "white")
-        hexagon.setAttribute("stroke", "gray");
-    } else {
-        hexagon.setAttribute("fill", "orange");
-    }
-    svg.id = shapeID
-
-    // Append the hexagon to the SVG element
-    svg.appendChild(hexagon);
-
-    // Append the SVG to the container in the HTML
-    document.getElementById(parentID).appendChild(svg);
+    hexagon = standardSVGSettings(hexagon, parentID, shapeID, svg)
 }
 
 function createSVGRhombus(shapeID, parentID) {
-    // Create an SVG namespace element
+
     const svgNS = "http://www.w3.org/2000/svg";
-
-    // Create an SVG element
     const svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "100");
-    svg.setAttribute("height", "100");
-
-    // Create a polygon for the rhombus
-    const rhombus = document.createElementNS(svgNS, "polygon");
-
-    // Define the points of the rhombus that fills the 100x100 box
+    let rhombus = document.createElementNS(svgNS, "polygon");
     const points = "50,5 95,50 50,95 5,50";
 
     rhombus.setAttribute("points", points);
-
-    // Set a fill color and stroke for the rhombus
-    rhombus.setAttribute("stroke-width", "2");
-    if (parentID === 'shapes') {
-        dark ? rhombus.setAttribute("fill", "black") : rhombus.setAttribute("fill", "white")
-        rhombus.setAttribute("stroke", "gray");
-    } else {
-        rhombus.setAttribute("fill", "orange");
-    }
-    svg.id = shapeID
-
-    // Append the rhombus to the SVG element
-    svg.appendChild(rhombus);
-
-    // Append the SVG to the container in the HTML
-    document.getElementById(parentID).appendChild(svg);
+    rhombus = standardSVGSettings(rhombus, parentID, shapeID, svg)
 }
 
 function createSVGSquare(shapeID, parentID) {
-    // Create an SVG namespace element
+
     const svgNS = "http://www.w3.org/2000/svg";
-
-    // Create an SVG element
     const svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "100");
-    svg.setAttribute("height", "100");
+    let square = document.createElementNS(svgNS, "rect");
 
-    // Create a rectangle for the square shape
-    const square = document.createElementNS(svgNS, "rect");
-
-    // Define the position and size of the square
     square.setAttribute("x", "5");
     square.setAttribute("y", "5");
     square.setAttribute("width", "90");
     square.setAttribute("height", "90");
 
-    // Set a fill color and stroke for the square
-    square.setAttribute("stroke-width", "2");
-    if (parentID === 'shapes') {
-        dark ? square.setAttribute("fill", "black") : square.setAttribute("fill", "white")
-        square.setAttribute("stroke", "gray");
-    } else {
-        square.setAttribute("fill", "orange");
-    }
-    svg.id = shapeID
-
-    // Append the square to the SVG element
-    svg.appendChild(square);
-
-    // Append the SVG to the container in the HTML
-    document.getElementById(parentID).appendChild(svg);
+    square = standardSVGSettings(square, parentID, shapeID, svg)
 }
